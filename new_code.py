6c97222b459d3632156fa6764bb478dd586f52da
Rank1one <bibhudatta.rank@gmail.com>
2019-07-15 11:05:56 +0530

    divide func added

--- new_code.py
+++ new_code.py
@@ -1,4 +1,10 @@
-def func(a,b):
+def mul(a,b):
     if a == b:
         return a**2
     return a*b
+
+def divide(a,b):
+    if b == 0:
+        raise ZeroDivisionError('b can not be 0')
+    else:
+        return a/b
